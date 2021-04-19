4e6cf4fd7c55060e88db3f023f7ce5be275f547a
Nathaniel Balderramas <52973151+nathabmmb@users.noreply.github.com>
2021-04-19 11:29:11 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import random
+"""
+Esto es lo que se conoce como un docstring, acá va una descripción breve del programa!.
+Se pueden escribir muchas lineas ;)
+"""
+
+
+#DATOS
+print("Este es un bosquejo del esqueleto del código")
+jugador1 = input("Ingrese el nombre del jugador 1: ")
+jugador2 = input("Ingrese el nombre del jugador 2: ")
+
+#PROCESO
+#Ronda 1
+#Jugador1
+#Jugador2
+#Ronda 2
+#Jugador1
+#Jugador2
+#Calculo de puntajes
+
+ganador = random.choice([jugador1, jugador2]) #modificar!
+#RESULTADO
+print("El ganador es: " + ganador +"! Felicidades!!")
